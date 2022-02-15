cdbbaabfd72c07b92b7757d795b58942890780c8
sarah358 <njerinjuguna358@gmail.com>
2022-02-15 17:41:02 +0300

    final

--- js/script.js
+++ js/script.js
@@ -10,5 +10,16 @@ $(document).ready(function(){
 
 
     });
+    // smooth scrolling
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        );
+    });
 
 });
